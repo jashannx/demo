497716b4b866c7c50024f4f3c6eebefb3b40b7d3
jass <your-email@example.com>
2025-09-30 15:13:56 +0530

    First commit

---
+++ Documents/react/vite-project/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css"
+function Title() {
+  return <h1>Welcome to React</h1>
+}
+function App() {
+  return (
+    <div>
+      <Title />
+      <p>This is a simple React application.</p>
+    </div>
+  );
+}
+
+export default App
